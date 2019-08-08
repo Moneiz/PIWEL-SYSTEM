c3c47558eb9e5f566db8d6534095bb53e8153eea
Atomix85 <bretellealan@hotmail.fr>
2019-08-08 22:23:51 +0200

    add gdt

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "gdt.h"
 
 void printf(char* str){
 
@@ -24,5 +25,6 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber){
 
 	printf("Hello World!");
 
+	GlobalDescriptorTable gdt;
 	while(1);
 }
